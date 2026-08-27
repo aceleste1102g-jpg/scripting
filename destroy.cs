237fe62448e9a3262fa474fc67ee97f38b4cd853
aceleste1102g-jpg <aceleste1102g@gmail.com>
2026-08-27 12:34:28 -0400

    Add files via upload

---
+++ destroy.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class destroyed : MonoBehaviour
+{
+    // destroy objet
+    void Start()
+    {
+        Destroy(gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
